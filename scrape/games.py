084b6e9403048354d5734da32e4a6a7e1cf6d41e
Declan Kelly <dkelly.home@gmail.com>
2018-11-28 07:54:20 +0000

    Add more winner checking

--- scrape/games.py
+++ scrape/games.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 from .common import simple_get
+from string import whitespace
+
+TO_REMOVE = whitespace + '★'
 
 def parse_game(raw_html):
     try:
@@ -19,11 +22,23 @@ def parse_game(raw_html):
         game = html.find_all('div', class_='portlet-body')[3] #for move list
 
         players = html.find_all('div', class_ = "col-xs-6 col-md-6")
-        black = players[0].find('a').get_text()
-        white = players[1].find('a').get_text()
+        black = players[0].find('a').get_text().strip(TO_REMOVE)
+        white = players[1].find('a').get_text().strip(TO_REMOVE)
 
         black_rating = players[0].find_all('br')[1].get_text().strip()
         white_rating = players[1].find_all('br')[1].get_text().strip()
+
+        # black_outcome = int(players[0].parent.find_all('span')[0].get_text().strip())
+        # white_outcome = int(players[1].parent.find_all('span')[0].get_text().strip())
+        
+        # winner = None
+        # if black_outcome > white_outcome:
+        #     winner = 'black'
+        # elif black_outcome < white_outcome:
+        #     winner = 'white'
+        # else:
+        #     # Since hex is proven to never end in a draw, exit immediately
+        #     return []
 
         moves = game.find_all('b')
         move_list = []
@@ -36,18 +51,30 @@ def parse_game(raw_html):
             move_list.append(moves[1].get_text().split(".",1)[1])
 
         turn = "black"
+        winner = None
+        resign = False
         for move in moves[2:]:
             m = move.get_text().split(".",1)[1]
 
             if m != "resign": # resign means they ended the game, not that the other player necessarily won
                 move_list.append(m)
 
+                resign = True
+
             if turn == "black":
                 turn = "white"
-            else: turn = "black"
+            else:
+                turn = "black"
 
-        winner = turn #should check if they're the winner, the other resigned, too few moves, or the game is unfinished
+        if resign:
+            winner = turn
+        else:
+            if turn == "black":
+                winner = "white"
+            else:
+                winner = "black"
+
         move_string = ''.join(move_list)
         return [gid,black,white,black_rating,white_rating,move_string,winner]
     except: 
-        return []    
+        return []
