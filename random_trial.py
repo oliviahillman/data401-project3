b40dd281cc6d9d8bde42988fe10b6432839820dd
Declan Kelly <dkelly.home@gmail.com>
2018-12-12 15:20:36 -0800

    Random trials impl

--- random_trial.py
+++ random_trial.py
@@ -2,37 +2,79 @@
 
 from player import *
 
-from hex.player import HumanPlayer
+from hex.player import RandomPlayer, HumanPlayer
 from collections import Counter
 import random
-from models.declan import DeclanModel
-from models.natalie import NatalieModel
+from models.natalie_random import NatalieRandomModel
+from models.natalie_random_nopool import NatalieRandomNoPoolModel
 from player import ModelPlayer, convert_game_fixed_to_relative
 from display import animate_board_choices
 
+import pandas as pd
 from tensorflow.train import checkpoint_exists
 
 import os
+import itertools as it
 
 BLACK = 1
 WHITE = 2
 
+NUM_EVAL_TRIALS = 250
+
 def run():
-    declan_model = DeclanModel()
-    if checkpoint_exists('checkpoints/declan_model_weights'):
-        declan_model.load('checkpoints/declan_model_weights')
+    model_defs = [
+        { 
+            'name': 'natalie_random',
+            'class': NatalieRandomModel,
+            'weights': 'weights/natalie_model_random_weights' },
+        {
+            'name': 'natalie_random_nopool',
+            'class': NatalieRandomNoPoolModel,
+            'weights': 'weights/natalie_model_random_nopool_weights' }
+    ]
 
-    natalie_model = NatalieModel()
+    random_player = RandomPlayer()
+    
+    for model_def in model_defs:
+        model = model_def['class']()
+        model.load(model_def['weights'])
+        
+        model_player = ModelPlayer(model)
+        index = pd.MultiIndex.from_tuples(
+            list(it.product(*[['random', model_def['name']], ['black', 'white']])),
+            names=['model', 'color'])
+        win_record = pd.Series(index=index, dtype=np.int16)
+        for r in range(NUM_EVAL_TRIALS):
+            switch = bool(random.getrandbits(1))
+            if switch:
+                p_black = model_player
+                p_white = random_player
+            else:
+                p_black = random_player
+                p_white = model_player
 
-    declan_player = ModelPlayer(declan_model, relative=True)
-    natalie_player = ModelPlayer(natalie_model)
-    random_player = HumanPlayer()
-
-    new_boards, new_winners, final_winner = play_game(declan_player,
-                                                      random_player,
-                                                      verbose=True,
-                                                      relative=False)
-
+            _, _, win_color = play_game(p_black, p_white,
+                                           verbose=False,
+                                           relative=False)
+            
+            if switch:
+                if win_color == 'black':
+                    win_name = model_def['name']
+                else:
+                    win_name = 'random'
+            else:
+                if win_color == 'white':
+                    win_name = model_def['name']
+                else:
+                    win_name = 'random'
+            
+            win_record[win_name, win_color] += 1
+        
+        print(win_record)
+        print(win_record.groupby(level=0).sum())
+        print(win_record.groupby(level=1).sum())
+            
+            
 if __name__ == '__main__':
     run()
 
